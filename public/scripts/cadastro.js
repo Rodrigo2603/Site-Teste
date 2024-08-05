1994b469d9db0bb37f6125e18e93735b5ce826d8
Rodrigo Rocha <rodrigorocha1032@gmail.com>
2024-08-05 18:58:06 -0300

    lógica para receber os valores submetidos no cadastro

--- public/scripts/cadastro.js
+++ public/scripts/cadastro.js
@@ -1,1 +1,19 @@
-Em desenvolvimento...
+document.addEventListener("DOMContentLoaded", function()
+{
+    const form = document.getElementById('cadastro');
+
+    form.addEventListener('submit', function(event)
+    {
+        event.preventDefault();
+
+        const Data = new FormData(form);
+
+        const senha = Data.get('senha');
+        const confirmaSenha = Data.get('confirmaSenha');
+
+        if (senha !== confirmaSenha) {
+            document.getElementById('resposta').innerHTML = "As senhas não coincidem.";
+            return
+        }
+    });
+});
